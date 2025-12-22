b4fd645da679195648790a98e10fe14461d02175
duyvip453 <duyvip453@gmail.com>
2025-12-22 22:52:03 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -10,19 +10,26 @@ local window = UI:CreateWindow({
 });
 local tab = {
     Home = window:AddTab({
-        Title = "Tab Information"
+        Title = "farm"
     })
 };
 local v17 = UI.Options;
 local id = game.PlaceId;
 if (id == 76558904092080) then
     duyvip = true;
-else
-warn("Game này không được hỗ trợ")
-return
 end
 game:GetService("Players").LocalPlayer.Idled:connect(function()
     game:GetService("VirtualUser"):Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame);
     wait();
     game:GetService("VirtualUser"):Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame);
 end);
+_G.AutoFarm = false
+
+tab.Farm:AddToggle("AutoFarmToggle", {
+    Title = "Auto Farm",
+    Default = false,
+    Callback = function(Value)
+        _G.AutoFarm = Value
+        print("AutoFarm:", Value)
+    end
+})
