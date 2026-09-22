b7714db3086ca48db65ef5f501b285746981ddbd
duyvip453 <duyvip453@gmail.com>
2026-09-22 09:38:30 +0700

    Update backgroud.lua

--- backgroud.lua
+++ backgroud.lua
@@ -1,4 +1,4 @@
-ocal Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/main/source.lua", true))()
 
 local Window = Luna:CreateWindow({
 	Name = "BlackKing", -- This Is Title Of Your Window
