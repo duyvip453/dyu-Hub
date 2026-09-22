bc68b39adc3f2e5393512f5531011009d56935a5
duyvip453 <duyvip453@gmail.com>
2026-09-22 09:22:22 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,32 +4,13 @@ local url = "https://raw.githubusercontent.com/duyvip453/dyu-Hub/refs/heads/main
 local UILibrary = loadstring(game:HttpGet(url))()
 
 -- Sau đó mới gọi hàm
-local MyWindow = UILibrary:CreateWindow("SUPER HUB 2026")
+local MyWindow = UILibrary:CreateWindow("DYU HUB")
 -- 3. Tạo Các Mục Lớn
-local TabNguoiChoi = MyWindow:CreateTab("Người Chơi")
-local TabVuKhi = MyWindow:CreateTab("Vũ Khí")
+local TabNguoiChoi = MyWindow:CreateTab("event")
+
 
 -- 4. Tạo Các Mục Chức Năng (Code game nằm hoàn toàn ở đây)
 
 -- Chức năng cho mục Người Chơi
-TabNguoiChoi:CreateButton("Tăng Tốc Độ (Speed)", function()
-    -- Path chỉ định đối tượng LocalPlayer như bạn đã nói
-    local player = game.Players.LocalPlayer
-    if player.Character and player.Character:FindFirstChild("Humanoid") then
-        player.Character.Humanoid.WalkSpeed = 50
-    end
-end)
 
-TabNguoiChoi:CreateButton("Bật Nhảy Cao (Jump)", function()
-    local player = game.Players.LocalPlayer
-    if player.Character and player.Character:FindFirstChild("Humanoid") then
-        player.Character.Humanoid.JumpPower = 100
-        player.Character.Humanoid.UseJumpPower = true
-    end
-end)
-
--- Chức năng cho mục Vũ Khí
-TabVuKhi:CreateButton("Tự Động Bắn", function()
-    print("Code tự động bắn sẽ viết ở đây...")
-end)
 print("---> EXECUTOR HOẠT ĐỘNG BÌNH THƯỜNG! <---")
